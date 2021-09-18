c032dc669f5eb503bfce86066e25c4d199fb1bde
Ciapas-Linux <maxgon@o2.pl>
2021-09-18 14:21:42 +0200

    first commit

---
+++ app/src/main/java/com/example/irrlicht/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.irrlicht;
+
+import android.app.NativeActivity;
+import android.os.Bundle;
+
+public class MainActivity extends NativeActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
